18047053a0fb69a6f0f054a92cf2dd781a0d87e9
Durid Ahmad <ahmad.durid96@gmail.com>
2021-04-05 14:55:14 -0600

    util.py: finished writing the hashing functions

---
+++ util.py
@@ -0,0 +1,16 @@
+from pyprimesieve import primes_nth
+from functools import reduce
+
+def _hash_char(val: str) -> int:
+    if len(val) > 1:
+        raise ValueError("char length must be 1")
+    
+    idx = ord(val)
+    return primes_nth(idx)
+
+
+def _get_hash(val) -> int:
+    if isinstance(val, int):
+        return primes_nth(val)
+    elif isinstance(val, str):
+        return reduce(lambda x, y: x * _hash_char(y), val, 1)
